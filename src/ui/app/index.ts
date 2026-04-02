ef482a28ed10334ff5296e63bb1979ec1d182ec0
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-02 15:48:05 +0300

    refactor: split ui foundation from app surface

---
+++ src/ui/app/index.ts
@@ -0,0 +1,23 @@
+export type { BadgeOptions, BadgeVariant } from "../types";
+
+export {
+  ALERT_CLASS_MAP,
+  BADGE_CLASS_MAP,
+  BODY_CLASS,
+  BODY_CLASS_LOGIN,
+  FILTER_LABEL,
+  HEADER_CARD,
+  LOGIN_CARD,
+  MEETING_STATUS_BADGE_CLASS_MAP,
+  PAGE_WRAP,
+  PAGE_WRAP_NARROW,
+  STATUS_BADGE,
+  TABLE_CELL,
+  TABLE_HEADER_ROW,
+  THEME_TOGGLE_BUTTON,
+  TOPIC_TEXT,
+  TOPIC_TEXT_SM,
+  getMeetingStatusBadgeClass,
+} from "../styles";
+
+export { renderBadge } from "../badge.htmlisp";
---
+++ src/ui/foundation/index.ts
@@ -0,0 +1,69 @@
+export type {
+  ButtonOptions,
+  ButtonVariant,
+  DangerPanelOptions,
+  DisclosureOptions,
+  FieldOptions,
+  MetadataItem,
+  MetadataListOptions,
+  SectionHeaderOptions,
+  SelectFieldOptions,
+  SelectOption,
+  TextareaFieldOptions,
+  ToggleGroupOptions,
+  ToggleItem,
+} from "../types";
+
+export {
+  BUTTON_CLASS_MAP,
+  DANGER_PANEL,
+  DANGER_PANEL_COMPACT,
+  DANGER_TEXT,
+  DANGER_TEXT_SM,
+  DANGER_TITLE,
+  DANGER_TITLE_SM,
+  DISCLOSURE,
+  DISCLOSURE_CONTENT,
+  DISCLOSURE_SUMMARY,
+  EMPTY_DASHED_CARD,
+  EMPTY_STATE_CARD,
+  FIELD_CONTROL,
+  FIELD_CONTROL_SM,
+  FIELD_CONTROL_WITH_MARGIN,
+  FIELD_LABEL,
+  FOCUS_RING,
+  FORM_LABEL,
+  FORM_STACK,
+  INSET_SURFACE_CARD,
+  METADATA_GRID,
+  METADATA_TERM,
+  METADATA_TILE,
+  METADATA_VALUE,
+  MUTED_TEXT,
+  MUTED_TEXT_XS,
+  PANEL_STACK,
+  SECTION_HEADER_ROW,
+  SECTION_META_TEXT,
+  SECTION_STACK_SM,
+  SOFT_SURFACE_CARD,
+  SUBTLE_TEXT,
+  SURFACE_CARD,
+  SURFACE_CARD_SM,
+  TEXT_LINK,
+  TOGGLE_BUTTON_META,
+  TOGGLE_BUTTON_PANEL,
+  TOGGLE_BUTTON_SEGMENTED,
+  TOGGLE_GROUP_SEGMENTED,
+} from "../styles";
+
+export { renderButton } from "../button.htmlisp";
+export { renderCard, renderCompactCard, renderInsetCard } from "../card.htmlisp";
+export { renderDangerPanel } from "../danger-panel.htmlisp";
+export { renderDisclosure } from "../disclosure.htmlisp";
+export { renderFieldShell } from "../field-shell.htmlisp";
+export { renderInputField } from "../input-field.htmlisp";
+export { renderMetadataList } from "../metadata-list.htmlisp";
+export { renderSectionHeader } from "../section-header.htmlisp";
+export { renderSelectField } from "../select-field.htmlisp";
+export { renderTextareaField } from "../textarea-field.htmlisp";
+export { renderToggleGroup } from "../toggle-group.htmlisp";
